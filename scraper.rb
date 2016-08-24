049d6f50f9e08f739e806e70557669a8ba41908f
Luke Bacon <luke@equivalentideas.com>
2016-08-24 16:03:33 +1000

    Parse in the text as an argument and call the method

--- scraper.rb
+++ scraper.rb
@@ -11,12 +11,13 @@ planningAlerts_subscribers_data = JSON.parse(
 )
 # build the sentence with new sign up stats
 # post it fortnightly
+post_message_to_slack(text)
 
-def post_message_to_slack
+def post_message_to_slack(text)
   request_body = {
     channel: "#bottesting",
     username: "webhookbot",
-    text: "This is posted to #bottesting and comes from a bot named webhookbot."
+    text: text
   }
 
   RestClient.post(ENV["SLACK_CHANNEL_WEBHOOK_URL"], request_body.to_json)
