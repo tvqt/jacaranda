a3de2eeb5391f7b9131604f8e7695bb29e7e55d5
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-08-14 09:11:58 +1000

    Fix naming

--- spec/planningalerts_spec.rb
+++ spec/planningalerts_spec.rb
@@ -9,21 +9,21 @@ describe 'PlanningAlerts' do
 
   # rubocop:disable LineLength
   it 'produces status text for new subscribers' do
-    VCR.use_cassette('planning_alerts_subscribers') do
+    VCR.use_cassette('planningalerts_subscribers') do
       text = PlanningAlerts.new_subscribers_text(period: last_fortnight)
       expect(text).to eq('714 people signed up for PlanningAlerts last fortnight :revolving_hearts: That’s 6% less than the fortnight before.')
     end
   end
 
   it 'produces status text for new unsubscribers' do
-    VCR.use_cassette('planning_alerts_subscribers') do
+    VCR.use_cassette('planningalerts_subscribers') do
       text = PlanningAlerts.new_unsubscribers_text(period: last_fortnight)
       expect(text).to eq('171 people left. That’s 40% less than the fortnight before.')
     end
   end
 
   it 'produces status text for total subscribers' do
-    VCR.use_cassette('planning_alerts_subscribers', record: :new_episodes) do
+    VCR.use_cassette('planningalerts_subscribers') do
       text = PlanningAlerts.total_subscribers_text
       expect(text).to eq('There are now 52,700 PlanningAlerts subscribers! :star2:')
     end
