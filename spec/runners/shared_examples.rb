8ab956cf0171292997dd2af510067ef84e60137a
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-09-09 20:25:34 +1000

    Remove duplicate tests, now they're on Jacaranda::BaseRunner

--- spec/runners/shared_examples.rb
+++ spec/runners/shared_examples.rb
@@ -6,29 +6,9 @@ RSpec.shared_examples 'a runner' do
   let(:runner_name) { described_class.to_s.split('::').first.downcase }
   let(:url) { Faker::Internet.url('hooks.slack.com') }
   let(:cassette) { runner_name + '_post_to_slack_webhook' }
-  let(:runner_env_post_day) { "MORPH_RUNNERS_#{runner_name.upcase}_POST_DAY" }
 
   before(:each) do
     set_environment_variable('MORPH_SLACK_CHANNEL_WEBHOOK_URL', url)
-  end
-
-  context 'when nominating a day to run' do
-    let(:days_of_week) { (0..6).map { |i| (Date.today + i).strftime('%A') } }
-
-    it 'has a default day' do
-      expect(described_class.post_day).to_not be nil
-    end
-
-    it 'can be set by environment variable', :aggregate_failures do
-      days_of_week.each do |day_name|
-        set_environment_variable(runner_env_post_day, day_name)
-        expect(described_class.post_day).to eq(day_name)
-      end
-    end
-
-    it 'validates the day'
-
-    after(:each) { restore_env }
   end
 
   context 'on nominated day' do
